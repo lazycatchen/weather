4c8f5143baf95df1a688a1808f12fadd4e185720
lazycatchen <chen_shuai_qi@126.com>
2019-07-28 20:57:37 +0800

    Signed-off-by: lazycatchen <chen_shuai_qi@126.com>

--- mysqltest.py
+++ mysqltest.py
@@ -5,30 +5,33 @@ db = pymysql.connect("localhost","root","31243124","test")
 cursor = db.cursor()
 
 class dbexec(object):
-    def __init__(self,FIRST_NAME,LAST_NAME, AGE, SEX, INCOME):
-        self.FIRST_NAME=FIRST_NAME
-        self.LAST_NAME=LAST_NAME
-        self.AGE=AGE
-        self.SEX=SEX
-        self.INCOME=INCOME
-    def adddata(self):
+    def __init__(self):
+        self.FIRST_NAME='MACC'
+        self.LAST_NAME='chen'
+        self.AGE=10
+        self.SEX='M'
+        self.INCOME=100
+    def adddata(self,FIRST_NAME,LAST_NAME,AGE,SEX,INCOME):
         print(self.AGE)
-        sql_2 = "INSERT INTO learnsql_1(FIRST_NAME,LAST_NAME, AGE, SEX, INCOME) VALUES(\'%s\',\'%s\',%d,\'%s\',%d)" %(self.FIRST_NAME,self.LAST_NAME,
-                                                                                                                      self.AGE,self.SEX,self.INCOME)
+        sql_2 = "INSERT INTO learnsql_1(FIRST_NAME,LAST_NAME, AGE, SEX, INCOME) VALUES(\'%s\',\'%s\',%d,\'%s\',%d)" %(FIRST_NAME,LAST_NAME,
+                                                                                                                      AGE,SEX,INCOME)
         #sql_2 = "INSERT INTO learnsql_1(FIRST_NAME,LAST_NAME, AGE, SEX, INCOME) VALUES ('%s' %self.FIRST_NAME+, '陈', 20, 'M', 2000)"
         cursor.execute(sql_2)
         db.commit()
-db0728=dbexec('Maca', '陈', 20, 'M', 2000)
-db0729=dbexec('Macx', '陈', 20, 'M', 2000)
+    def deletedata(self,FIRST_NAME):
+        sql = "delete from learnsql_1 where FIRST_NAME='%s'" % (FIRST_NAME)
+        print(sql)
+        cursor.execute(sql)
+        db.commit()
 
 
 # 使用 execute() 方法执行 SQL，如果表存在则删除
 #cursor.execute("DROP TABLE IF EXISTS learnsql_1")
 
 try:
-    #cursor.execute(sql)
-    db0728.adddata()
-    db0729.adddata()
+    db0728 = dbexec()
+    db0728.adddata('Macas', '陈', 20, 'M', 2000)
+    db0728.deletedata('Maca1')
 except:
     db.rollback()
 
